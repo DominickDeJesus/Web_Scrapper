9e7c4e793d660c1f96e9b024a0a184e31af994e6
DominickDeJesus <dominick.dejesus@gmail.com>
2020-10-06 23:48:17 -0400

    fixed add bug

--- client/src/components/TrackCard.jsx
+++ client/src/components/TrackCard.jsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Button } from 'react-bootstrap';
 import { FaPlay, FaPlus } from 'react-icons/fa';
+import { AppContext } from '../context/AppContext';
+const TrackCard = ({ track }) => {
+  const { tracks, setTracks } = useContext(AppContext);
 
-const TrackCard = ({ track, handleClick }) => {
+  const handleClick = (event) => {
+    console.log(event.currentTarget);
+    console.log(tracks, event.currentTarget.getAttribute('name'));
+    const obj = tracks.find(
+      (track) => String(track._id) === String(event.currentTarget.name)
+    );
+    console.log(obj);
+    const index = tracks.indexOf(obj);
+    console.log(index);
+    console.log(event.currentTarget.name);
+    if (index !== -1) {
+      setTracks([...tracks, tracks[index]]);
+      console.log(tracks);
+    }
+  };
+
   return (
     <div className="d-flex flex-direction-row w-100 border m-2 p-1">
       <Button
@@ -16,7 +34,7 @@ const TrackCard = ({ track, handleClick }) => {
         className="ml-auto d-flex justify-content-center align-items-center"
         onClick={handleClick}
         variant="flat"
-        name={track?._id}
+        name={track._id}
       >
         <FaPlus />
       </Button>
